7811a0591e87aa7f8d3a722bdd00b3a936b56903
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-11-11 08:58:12 +0700

    Create 9.java

---
+++ Java/Java III/9.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    Person.hello();
+  }
+}
+
+class Person {
+  public static void hello() {
+    // Gunakan "Hello World" dengan "Hello Java"
+    System.out.println("Hello Java");
+  }
+}
