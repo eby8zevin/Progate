92e055e109d15b037165ff778cddc46e4c66408b
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-11-13 14:11:39 +0700

    Create 3.php
    Kalkulasi

---
+++ PHP/PHP I/Latihan/3.php
@@ -0,0 +1,16 @@
+//index.php
+<?php
+$price = 10;
+$taxRate = 0.08;
+echo 'Nilai $price: '.$price;
+echo '<br>';
+echo 'Nilai $taxRate: '.$taxRate;
+echo '<br>';
+echo '-----';
+echo '<br>';
+
+// Ketik code Anda dibawah
+$taxIncludedPrice = $price + $price * $taxRate;
+echo 'Harga setelah pajak adalah $'.$taxIncludedPrice;
+
+?>
