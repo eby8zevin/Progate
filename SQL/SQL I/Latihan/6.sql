051e4470e763a0c3094e48cdafee98269a12f3c3
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-11-14 15:08:20 +0700

    Create 6.sql
    Riwayat Penjualan

---
+++ SQL/SQL I/Latihan/6.sql
@@ -0,0 +1,17 @@
+//e1
+-- dapatkan jumlah berapa kali setiap jenis item terjual berdasarkan id item
+SELECT item_id, COUNT(*)
+FROM sales_records
+GROUP BY item_id;
+
+//e2
+/*
+dapatkan id dan jumlah penjualan per unitnya dari 5 barang paling populer.
+Susun hasilnya secara menurun
+*/
+
+SELECT item_id, COUNT(*)
+FROM sales_records
+GROUP BY item_id
+ORDER BY COUNT(*) DESC
+LIMIT 5;
