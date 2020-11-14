4b4e80b7125898b1003964dcb7c455e3f24b588f
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-11-14 15:48:32 +0700

    Create 9.sql
    GROUP BY (WHERE)

---
+++ SQL/SQL II/9.sql
@@ -0,0 +1,22 @@
+//e1
+/*
+tambahkan aturan di klausa WHERE untuk mengelompokkan
+baris, dimana character_name adalah "Ninja Ken"
+*/
+
+SELECT SUM(price), purchased_at
+FROM purchases
+WHERE character_name = "Ninja Ken"
+GROUP BY purchased_at;
+
+//e2
+/*
+Tambahkan klausa WHERE untuk mendapatkan baris dengan category "makanan"
+setelah itu, kelompokkan hasilnya berdasarkan kolom
+purchased_at dan character_name 
+*/
+
+SELECT SUM(price), purchased_at, character_name
+FROM purchases
+WHERE category = "makanan"
+GROUP BY purchased_at, character_name;
